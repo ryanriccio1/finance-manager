cc5dddd855b3f02473618f8534707760add95e39
Ryan Riccio <r.sharpbard@gmail.com>
2022-10-24 13:18:34 -0700

    Added UI

--- TeamProject/APIUI/MainForm.Designer.cs
+++ TeamProject/APIUI/MainForm.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_Get = new System.Windows.Forms.Button();
+            this.rtb_Output = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // btn_Get
+            // 
+            this.btn_Get.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Get.Location = new System.Drawing.Point(15, 28);
+            this.btn_Get.Name = "btn_Get";
+            this.btn_Get.Size = new System.Drawing.Size(1202, 176);
+            this.btn_Get.TabIndex = 0;
+            this.btn_Get.Text = "GET";
+            this.btn_Get.UseVisualStyleBackColor = true;
+            // 
+            // rtb_Output
+            // 
+            this.rtb_Output.Location = new System.Drawing.Point(15, 210);
+            this.rtb_Output.Name = "rtb_Output";
+            this.rtb_Output.Size = new System.Drawing.Size(1202, 592);
+            this.rtb_Output.TabIndex = 1;
+            this.rtb_Output.Text = "";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1233, 814);
+            this.Controls.Add(this.rtb_Output);
+            this.Controls.Add(this.btn_Get);
             this.Name = "MainForm";
             this.Text = "API Interface";
             this.ResumeLayout(false);
@@ -42,6 +64,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_Get;
+        private System.Windows.Forms.RichTextBox rtb_Output;
     }
 }
 
