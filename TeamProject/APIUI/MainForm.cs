706c484763a030ef0fde6690d589ee78217e3aa2
Ryan Riccio <r.sharpbard@gmail.com>
2022-10-24 13:39:18 -0700

    Added Chuck Norris API Integration

--- TeamProject/APIUI/MainForm.cs
+++ TeamProject/APIUI/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RestSharp;
 
 namespace APIUI
 {
@@ -15,6 +16,20 @@ namespace APIUI
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void btn_Get_Click(object sender, EventArgs e)
+        {
+            var client = new RestClient("https://api.chucknorris.io/jokes/random"); // store the client to perform GET request with
+            var request = new RestRequest();    // the request does not need any data
+            var response = client.Get(request); // GET request from client
+
+            // make sure we got successful data
+            if (!response.IsSuccessStatusCode) return;
+
+            // display raw data
+            string rawResponse = response.Content;
+            rtb_Output.Text = rawResponse;
         }
     }
 }
